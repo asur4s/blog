33571371c81b97e6e8e739f32aa3ad50a5877c6f
asur4s <asur4s@126.com>
2022-04-07 03:26:01 +0000

    rust finish 3.1

--- code/guessing_game/src/main.rs
+++ code/guessing_game/src/main.rs
@@ -1,25 +1,32 @@
-use std::io;    // prelude 预导入模块
 use rand::Rng; // trait 类似接口
+use std::cmp::Ordering;
+use std::io; // prelude 预导入模块
 
 fn main() {
     println!("猜数！");
-    println!("猜一个数");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("secret number {}", secret_number);
 
-    // let mut foo = 1;
-    // let bar = foo;  // immutable
-    // foo = 2;
+    loop {
+        println!("猜一个数");
+        // UTF8，::代表 String 的关联函数。
+        let mut guess = String::new();
+        // io::Result: Ok（数据）, Err。Result必须处理，否则会被警告。
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+        // 类型转换，parse 可以解析成整数。优雅的异常处理
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    // 可变变量
-    // UTF8，::代表 String 的关联函数。
-    let mut guess = String::new();
-    // & 表示引用，方法按引用进行传递。
-    // &guess 默认不可变，所以必须加入 mut。
-    io::stdin().read_line(&mut guess).expect("无法读取行");
-    // io::Result: Ok（数据）, Err
-    // Result必须处理，否则会被警告。
-
-    println!("你猜测的数是：{}", guess);
+        println!("你猜测的数是：{}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
